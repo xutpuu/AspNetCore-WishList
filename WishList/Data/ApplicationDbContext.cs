30d11f608ad9add43149725af578ce0a14dfd03c
xutpuu <xutpuu@gmail.com>
2018-09-27 12:26:26 +0300

    Completed the module

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Item> Items { get; set; };
+        public DbSet<Item> Items { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
