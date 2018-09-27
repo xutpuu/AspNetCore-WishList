153b1c7b12784f4a80d9802dbaeb3dc8b01842cd
xutpuu <xutpuu@gmail.com>
2018-09-27 12:23:35 +0300

    Completed the module

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+    }
+}
